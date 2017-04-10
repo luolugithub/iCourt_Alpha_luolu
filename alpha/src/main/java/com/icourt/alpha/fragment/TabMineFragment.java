9604218e47f592203eac5c8b4514128ab33c46ab
zhaolu <zhaolu@icourt.cc>
2017-04-10 11:29:24 +0800

    个人中心布局

--- alpha/src/main/java/com/icourt/alpha/fragment/TabMineFragment.java
+++ alpha/src/main/java/com/icourt/alpha/fragment/TabMineFragment.java
@@ -2,16 +2,20 @@ package com.icourt.alpha.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.RatingBar;
-import android.widget.SeekBar;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.icourt.alpha.R;
+import com.icourt.alpha.activity.AboutActivity;
 import com.icourt.alpha.activity.UpdatePhoneOrMailActivity;
 import com.icourt.alpha.base.BaseFragment;
+import com.icourt.alpha.utils.GlideUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,16 +31,56 @@ import butterknife.Unbinder;
  */
 public class TabMineFragment extends BaseFragment {
 
-
-    @BindView(R.id.seekBar)
-    SeekBar seekBar;
-    @BindView(R.id.ratingBar)
-    RatingBar ratingBar;
-    @BindView(R.id.bt_phone)
-    Button btPhone;
-    @BindView(R.id.bt_email)
-    Button btEmail;
+    @BindView(R.id.titleBack)
+    ImageView titleBack;
+    @BindView(R.id.titleContent)
+    TextView titleContent;
+    @BindView(R.id.titleView)
+    AppBarLayout titleView;
     Unbinder unbinder;
+    @BindView(R.id.my_center_photo_view)
+    ImageView myCenterPhotoView;
+    @BindView(R.id.my_center_photo_layout)
+    FrameLayout myCenterPhotoLayout;
+    @BindView(R.id.my_center_name_view)
+    TextView myCenterNameView;
+    @BindView(R.id.my_center_lawplace_view)
+    TextView myCenterLawplaceView;
+    @BindView(R.id.my_center_phone_textview)
+    TextView myCenterPhoneTextview;
+    @BindView(R.id.my_center_phone_layout)
+    LinearLayout myCenterPhoneLayout;
+    @BindView(R.id.my_center_mail_textview)
+    TextView myCenterMailTextview;
+    @BindView(R.id.my_center_mail_layout)
+    LinearLayout myCenterMailLayout;
+    @BindView(R.id.my_center_collect_layout)
+    LinearLayout myCenterCollectLayout;
+    @BindView(R.id.my_center_at_layout)
+    LinearLayout myCenterAtLayout;
+    @BindView(R.id.my_center_file_layout)
+    LinearLayout myCenterFileLayout;
+    @BindView(R.id.my_center_clear_cache_layout)
+    LinearLayout myCenterClearCacheLayout;
+    @BindView(R.id.my_center_clear_about_layout)
+    LinearLayout myCenterClearAboutLayout;
+    @BindView(R.id.my_center_clear_loginout_layout)
+    LinearLayout myCenterClearLoginoutLayout;
+
+    @BindView(R.id.my_center_collect_textview)
+    TextView myCenterCollectTextview;
+    @BindView(R.id.my_center_at_textview)
+    TextView myCenterAtTextview;
+    @BindView(R.id.my_center_file_textview)
+    TextView myCenterFileTextview;
+    @BindView(R.id.my_center_clear_cache_textview)
+    TextView myCenterClearCacheTextview;
+    @BindView(R.id.my_center_about_count_view)
+    TextView myCenterAboutCountView;
+    @BindView(R.id.my_center_alpha_qiyu_msgcount_textview)
+    TextView myCenterAlphaQiyuMsgcountTextview;
+    @BindView(R.id.my_center_clear_help_layout)
+    LinearLayout myCenterClearHelpLayout;
 
     public static TabMineFragment newInstance() {
         return new TabMineFragment();
@@ -53,7 +97,15 @@ public class TabMineFragment extends BaseFragment {
 
     @Override
     protected void initView() {
-        log("==========initView:" + this);
+        titleBack.setVisibility(View.INVISIBLE);
+        titleContent.setText("个人中心");
+        if (GlideUtils.canLoadImage(this)) {
+            GlideUtils.loadUser(getContext(), "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1491804322903&di=06004cfeb2a47c19dfcca762851687dd&imgtype=0&src=http%3A%2F%2Fimage.coolapk.com%2Fapk_logo%2F2015%2F0703%2F257251_1435907700_3237.png", myCenterPhotoView);
+        }
+        myCenterNameView.setText("Alpha");
+        myCenterLawplaceView.setText("iCourt");
+        myCenterPhoneTextview.setText("18888887777");
+        myCenterMailTextview.setText("zhaolu@icourt.cc");
     }
 
     @Override
@@ -61,16 +113,34 @@ public class TabMineFragment extends BaseFragment {
 
     }
 
-    @OnClick({R.id.bt_phone, R.id.bt_email})
+    @OnClick({R.id.my_center_phone_layout, R.id.my_center_mail_layout, R.id.my_center_collect_layout, R.id.my_center_at_layout, R.id.my_center_file_layout, R.id.my_center_clear_cache_layout, R.id.my_center_clear_about_layout, R.id.my_center_clear_loginout_layout})
     @Override
     public void onClick(View v) {
         super.onClick(v);
         switch (v.getId()) {
-            case R.id.bt_phone:
-                UpdatePhoneOrMailActivity.launch(getContext(), UpdatePhoneOrMailActivity.UPDATE_PHONE_TYPE, "18888887777");
+            case R.id.my_center_phone_layout://电话
+                UpdatePhoneOrMailActivity.launch(getContext(), UpdatePhoneOrMailActivity.UPDATE_PHONE_TYPE, myCenterPhoneTextview.getText().toString().trim());
                 break;
-            case R.id.bt_email:
-                UpdatePhoneOrMailActivity.launch(getContext(), UpdatePhoneOrMailActivity.UPDATE_EMAIL_TYPE, "zhaolu@icourt.cc");
+            case R.id.my_center_mail_layout://邮箱
+                UpdatePhoneOrMailActivity.launch(getContext(), UpdatePhoneOrMailActivity.UPDATE_EMAIL_TYPE, myCenterMailTextview.getText().toString().trim());
+                break;
+            case R.id.my_center_collect_layout://收藏
+
+                break;
+            case R.id.my_center_at_layout://提及我的
+
+                break;
+            case R.id.my_center_file_layout://我的文件
+
+                break;
+            case R.id.my_center_clear_cache_layout://清除缓存
+
+                break;
+            case R.id.my_center_clear_about_layout://关于
+                AboutActivity.launch(getContext());
+                break;
+            case R.id.my_center_clear_loginout_layout://退出
+
                 break;
         }
     }
