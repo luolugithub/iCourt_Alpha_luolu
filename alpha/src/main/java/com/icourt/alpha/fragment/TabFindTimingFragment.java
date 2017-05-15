1cc5555e1f971598a1fbed25e3494b8bb42b5a1a
asange <youxuan@icourt.cc>
2017-05-15 12:56:38 +0800

    我的计时

--- alpha/src/main/java/com/icourt/alpha/fragment/TabFindTimingFragment.java
+++ alpha/src/main/java/com/icourt/alpha/fragment/TabFindTimingFragment.java
@@ -2,12 +2,32 @@ package com.icourt.alpha.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.icourt.alpha.R;
 import com.icourt.alpha.base.BaseFragment;
+import com.icourt.alpha.utils.SystemUtils;
+import com.icourt.alpha.view.xrefreshlayout.RefreshLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.ValueShape;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.LineChartView;
 
 /**
  * Description
@@ -18,19 +38,123 @@ import com.icourt.alpha.base.BaseFragment;
  */
 public class TabFindTimingFragment extends BaseFragment {
 
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
+    @BindView(R.id.timing_date_title)
+    TextView timingDateTitle;
+    @BindView(R.id.timing_count_total)
+    TextView timingCountTotal;
+    @BindView(R.id.timing_today_total)
+    TextView timingTodayTotal;
+    @BindView(R.id.timing_chart_view)
+    LineChartView timingChartView;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.refreshLayout)
+    RefreshLayout refreshLayout;
+    Unbinder unbinder;
+
     public static TabFindTimingFragment newInstance() {
         return new TabFindTimingFragment();
     }
 
+    private LineChartData data;
+    private int numberOfLines = 1;
+    private int maxNumberOfLines = 4;
+    private int numberOfPoints = 12;
+
+    float[][] randomNumbersTab = new float[maxNumberOfLines][numberOfPoints];
+
+    private boolean hasAxes = true;
+    private boolean hasLines = true;
+
+    private boolean hasPoints = false;//不要圆点
+    private ValueShape shape = ValueShape.CIRCLE;
+    private boolean isFilled = true;
+    private boolean hasLabels = false;
+    private boolean isCubic = true;
+    private boolean hasLabelForSelected = false;
+    private boolean hasGradientToTransparent = false;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(R.layout.fragment_tab_find_timing, inflater, container, savedInstanceState);
+        View view = super.onCreateView(R.layout.fragment_tab_find_timing, inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     protected void initView() {
+        tabLayout.addTab(tabLayout.newTab().setText("我的计时"), 0, true);
+        // Generate some random values.
+        generateValues();
+
+        generateData();
+
+        resetViewport();
+    }
+
+    private void generateValues() {
+        for (int i = 0; i < maxNumberOfLines; ++i) {
+            for (int j = 0; j < numberOfPoints; ++j) {
+                randomNumbersTab[i][j] = (float) ((int) (Math.random() * 24));
+            }
+        }
+    }
+
+
+    private void resetViewport() {
+        // Reset viewport height range to (0,100)
+        final Viewport v = new Viewport(timingChartView.getMaximumViewport());
+        v.bottom = 0;
+        v.top = 24;
+        v.left = 0;
+        v.right = numberOfPoints + 2;
+        timingChartView.setMaximumViewport(v);
+        timingChartView.setCurrentViewport(v);
+    }
+
+    private void generateData() {
+
+        List<Line> lines = new ArrayList<Line>();
+        for (int i = 0; i < numberOfLines; ++i) {
+
+            List<PointValue> values = new ArrayList<PointValue>();
+            for (int j = 0; j < numberOfPoints; ++j) {
+                values.add(new PointValue(j, randomNumbersTab[i][j]));
+            }
+
+            Line line = new Line(values);
+            line.setColor(ChartUtils.COLORS[i]);
+            line.setShape(shape);
+            line.setCubic(isCubic);
+            line.setFilled(isFilled);
+            line.setHasLabels(hasLabels);
+            line.setHasLabelsOnlyForSelected(hasLabelForSelected);
+            line.setHasLines(hasLines);
+            line.setHasPoints(hasPoints);
+            line.setColor(SystemUtils.getColor(getContext(), R.color.alpha_font_color_orange));
+            //line.setHasGradientToTransparent(hasGradientToTransparent);
+            lines.add(line);
+        }
+
+        data = new LineChartData(lines);
+
+        Axis axisX = new Axis();
+        Axis axisY = new Axis().setHasLines(true);
+        data.setAxisXBottom(axisX);
+        data.setAxisYLeft(axisY);
+
+        data.setBaseValue(Float.NEGATIVE_INFINITY);
+        timingChartView.setLineChartData(data);
 
     }
 
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
