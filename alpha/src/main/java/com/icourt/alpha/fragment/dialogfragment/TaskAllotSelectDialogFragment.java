d3f25cefb8a4bc73beab760d2f5c0cc6c97eba8e
zhaolu <zhaolu@icourt.cc>
2017-09-06 15:05:28 +0800

    所有gravity为bottom的dialogfragment添加自下而上的动画效果

--- alpha/src/main/java/com/icourt/alpha/fragment/dialogfragment/TaskAllotSelectDialogFragment.java
+++ alpha/src/main/java/com/icourt/alpha/fragment/dialogfragment/TaskAllotSelectDialogFragment.java
@@ -109,6 +109,7 @@ public class TaskAllotSelectDialogFragment extends BaseDialogFragment implements
         if (dialog != null) {
             Window window = dialog.getWindow();
             if (window != null) {
+                window.setWindowAnimations(R.style.AppThemeSlideAnimation);
                 window.setGravity(Gravity.BOTTOM);
                 View decorView = window.getDecorView();
                 if (decorView != null) {
