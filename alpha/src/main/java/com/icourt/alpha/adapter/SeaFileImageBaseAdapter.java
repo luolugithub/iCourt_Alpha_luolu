9fb506d30f0e2803830327fb55444c6741ef6db2
吴佑炫 <youxuan@icourt.cc>
2017-09-12 17:54:14 +0800

    AD-86_文档模块开发_任务附件加载图片

---
+++ alpha/src/main/java/com/icourt/alpha/adapter/SeaFileImageBaseAdapter.java
@@ -0,0 +1,58 @@
+package com.icourt.alpha.adapter;
+
+import android.support.annotation.DrawableRes;
+import android.widget.ImageView;
+
+import com.icourt.alpha.BuildConfig;
+import com.icourt.alpha.adapter.baseadapter.BaseArrayRecyclerAdapter;
+import com.icourt.alpha.entity.bean.ISeaFileImage;
+import com.icourt.alpha.utils.FileUtils;
+import com.icourt.alpha.utils.GlideUtils;
+import com.icourt.alpha.utils.SFileTokenUtils;
+
+/**
+ * Description  sea file加载图片的适配器
+ * Company Beijing icourt
+ * author  youxuan  E-mail:xuanyouwu@163.com
+ * date createTime：2017/9/12
+ * version 2.1.0
+ */
+public abstract class SeaFileImageBaseAdapter<T extends ISeaFileImage> extends BaseArrayRecyclerAdapter<T> {
+    /**
+     * 加载图片
+     *
+     * @param iSeaFileImage
+     * @param view
+     */
+    protected void loadSFileImage(ISeaFileImage iSeaFileImage, ImageView view) {
+        if (iSeaFileImage == null) return;
+        if (view == null) return;
+        GlideUtils.loadSFilePic(view.getContext(), getSFileImageThumbUrl(iSeaFileImage), view);
+    }
+
+    /**
+     * 获取图片地址
+     *
+     * @param iSeaFileImage
+     * @return
+     */
+    protected String getSFileImageThumbUrl(ISeaFileImage iSeaFileImage) {
+        return String.format("%silaw/api/v2/documents/thumbnailImage?repoId=%s&seafileToken=%s&p=%s&size=%s",
+                BuildConfig.API_URL,
+                iSeaFileImage.getSeaFileImageRepoId(),
+                SFileTokenUtils.getSFileToken(),
+                iSeaFileImage.getSeaFileImageFullPath(),
+                150);
+    }
+
+    /**
+     * 获取文件对应图标
+     *
+     * @param fileName
+     * @return
+     */
+    @DrawableRes
+    protected int getSFileTypeIcon(String fileName) {
+        return FileUtils.getSFileIcon(fileName);
+    }
+}
