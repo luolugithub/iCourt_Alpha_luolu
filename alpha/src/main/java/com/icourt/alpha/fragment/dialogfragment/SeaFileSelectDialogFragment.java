de60a7e84eb54f7bdaa8a808b8d3f38c5615df27
吴佑炫 <youxuan@icourt.cc>
2017-09-18 15:27:49 +0800

    AD-86_文档模块_资料库id返回为null 追加日志

--- alpha/src/main/java/com/icourt/alpha/fragment/dialogfragment/SeaFileSelectDialogFragment.java
+++ alpha/src/main/java/com/icourt/alpha/fragment/dialogfragment/SeaFileSelectDialogFragment.java
@@ -208,6 +208,10 @@ public class SeaFileSelectDialogFragment extends BaseDialogFragment
                 @Override
                 public void onSuccess(Call<RepoIdResEntity> call, Response<RepoIdResEntity> response) {
                     dismissLoadingDialog();
+                    if (TextUtils.isEmpty(response.body().seaFileRepoId)) {
+                        showToast("服务器返回的资料库id为null");
+                        bugSync("项目repo 获取null", "projectid:" + projectId);
+                    }
                     //对应项目下的第一级文件
                     SeaFileSelectParam seaFileSelectParam = new SeaFileSelectParam(
                             REPO_PROJECT,
