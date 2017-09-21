6c5583d9d1b89a7fb289f779c948bdd45a524727
zhaodanyang <zhaodanyang@icourt.cc>
2017-09-21 18:57:31 +0800

    添加计时选择器的弹出窗，添加日的选择框。

--- alpha/src/main/java/com/icourt/alpha/fragment/TimingSelectDayFragment.java
+++ alpha/src/main/java/com/icourt/alpha/fragment/TimingSelectDayFragment.java
@@ -5,9 +5,21 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.icourt.alpha.R;
 import com.icourt.alpha.base.BaseFragment;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by zhaodanyang on 2017/9/21.
@@ -15,15 +27,61 @@ import com.icourt.alpha.base.BaseFragment;
 
 public class TimingSelectDayFragment extends BaseFragment {
 
+    @BindView(R.id.titleBack)
+    ImageView titleBack;
+    @BindView(R.id.titleContent)
+    TextView titleContent;
+    @BindView(R.id.titleForward)
+    ImageView titleForward;
+    @BindView(R.id.titleAction)
+    TextView titleAction;
+    @BindView(R.id.compactcalendar_view)
+    CompactCalendarView compactcalendarView;
+
+    Unbinder unbinder;
+
+    Date selectedDate;
+
+    private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("yyyy年MMM", Locale.getDefault());
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = super.onCreateView(R.layout.fragment_timing_select_day, inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
         return view;
     }
 
     @Override
     protected void initView() {
+        initCompactCalendar();
+    }
 
+    private void initCompactCalendar() {
+        compactcalendarView.setUseThreeLetterAbbreviation(false);
+        compactcalendarView.setLocale(TimeZone.getDefault(), Locale.CHINESE);
+        compactcalendarView.setUseThreeLetterAbbreviation(true);
+        compactcalendarView.setDayColumnNames(new String[]{"一", "二", "三", "四", "五", "六", "日"});
+        compactcalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date date) {
+                selectedDate = date;
+            }
+
+            @Override
+            public void onMonthScroll(Date date) {
+                titleContent.setText(dateFormatForMonth.format(date));
+            }
+        });
+        titleContent.setText(dateFormatForMonth.format(System.currentTimeMillis()));
+
+        compactcalendarView.removeAllEvents();
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
