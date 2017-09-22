4e4b310a158790f0ad2cf292cc2fff524cda8abd
赵潞 <zhaolu@icourt.cc>
2017-09-22 14:52:10 +0800

    缓存清理后显示0B

--- alpha/src/main/java/com/icourt/alpha/fragment/TabMineFragment.java
+++ alpha/src/main/java/com/icourt/alpha/fragment/TabMineFragment.java
@@ -277,7 +277,7 @@ public class TabMineFragment extends BaseFragment {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 DataCleanManager.clearAllCache(getLoginUserId());
-                                myCenterClearCacheTextview.setText("0K");
+                                myCenterClearCacheTextview.setText("0B");
                                 showTopSnackBar(R.string.my_center_clear_cache_succee_text);
                             }
                         }).setNegativeButton("取消", null)
