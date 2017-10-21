b48d4c5fa6f927aff3a59558d2a8037237c87a4d
吴佑炫 <youxuan@icourt.cc>
2017-10-21 10:42:38 +0800

    at 我的 算法优化,二级缓存

--- alpha/src/main/java/com/icourt/alpha/adapter/MyAtedAdapter.java
+++ alpha/src/main/java/com/icourt/alpha/adapter/MyAtedAdapter.java
@@ -47,11 +47,13 @@ import static com.netease.nimlib.sdk.msg.model.QueryDirectionEnum.QUERY_OLD;
  * version 1.0.0
  */
 public class MyAtedAdapter extends ContactBaseAdapter<IMMessageCustomBody> implements OnItemClickListener {
-    ArrayMap<String, RecentContact> localRecentContacts = new ArrayMap<>();//会话列表
+    ArrayMap<String, RecentContact> localRecentContacts = new ArrayMap<>();//会话列表 key contactid[云信]
+    ArrayMap<Long, Boolean> atReadedMessageMap = new ArrayMap<>();//消息读取状态, key msgid[alpha]
 
-
-    private int READ_TITLE_COLOR = 0xFFa6a6a6;
-    private int READ_CONTENT_COLOR = 0xFFa6a6a6;
+    private int READED_TITLE_COLOR = 0xFFa6a6a6;
+    private int READED_CONTENT_COLOR = READED_TITLE_COLOR;
+    private int READED_TIME_COLOR = 0xFFd3d3d3;
+    private int READED_GROUP_COLOR = READED_TIME_COLOR;
 
     public MyAtedAdapter() {
         this.setOnItemClickListener(this);
@@ -70,6 +72,7 @@ public class MyAtedAdapter extends ContactBaseAdapter<IMMessageCustomBody> imple
     @Override
     public boolean bindData(boolean isRefresh, @NonNull List<IMMessageCustomBody> datas) {
         if (isRefresh) {
+            atReadedMessageMap.clear();
             getRecentContacts();
         }
         return super.bindData(isRefresh, datas);
@@ -139,15 +142,47 @@ public class MyAtedAdapter extends ContactBaseAdapter<IMMessageCustomBody> imple
         at_user_tv.setText(imAtEntity.name);
         at_time_tv.setText(DateUtils.getFormatChatTimeSimple(imAtEntity.send_time));
         at_content_tv.setText(imAtEntity.content);
-        updateReadViewState(imAtEntity, at_user_iv, at_user_tv, at_content_tv);
+        updateReadViewState(imAtEntity, at_user_iv, at_user_tv, at_content_tv, at_from_group_tv, at_time_tv);
 
     }
 
-    private void updateReadViewState(@Nullable final IMMessageCustomBody imAtEntity, final ImageView at_user_iv, final TextView at_user_tv, final TextView at_content_tv) {
+    /**
+     * 更新 已读 未读状态
+     *
+     * @param imAtEntity
+     * @param at_user_iv
+     * @param at_user_tv
+     * @param at_content_tv
+     */
+    private void updateReadViewState(@Nullable final IMMessageCustomBody imAtEntity,
+                                     final ImageView at_user_iv,
+                                     final TextView at_user_tv,
+                                     final TextView at_content_tv,
+                                     final TextView at_from_group_tv,
+                                     final TextView at_time_tv) {
+        if (at_user_iv == null) {
+            return;
+        }
         //处理读取状态
+        //1. 判断会话
         RecentContact recentContact = localRecentContacts.get(imAtEntity.to);
         int sessionUnReadNum = recentContact == null ? 0 : recentContact.getUnreadCount();
         if (sessionUnReadNum > 0) {
+
+            //2. 判断缓存的消息状态列表
+            if (atReadedMessageMap.containsKey(imAtEntity.id)) {
+                Boolean aBoolean = atReadedMessageMap.get(imAtEntity.id);
+                updateReadedStateView(aBoolean != null && aBoolean.booleanValue(),
+                        at_user_iv,
+                        at_user_tv,
+                        at_content_tv,
+                        at_from_group_tv,
+                        at_time_tv,
+                        imAtEntity);
+                return;
+            }
+
+            //3. 从数据库拿
             IMMessage emptyMessage = MessageBuilder.createEmptyMessage(
                     imAtEntity.to,
                     imAtEntity.ope == CHAT_TYPE_P2P ? SessionTypeEnum.P2P : SessionTypeEnum.Team,
@@ -163,40 +198,67 @@ public class MyAtedAdapter extends ContactBaseAdapter<IMMessageCustomBody> imple
                                     if (imMessage == null) {
                                         continue;
                                     }
+                                    //缓存
+                                    atReadedMessageMap.put(imAtEntity.id, imMessage.getStatus() == MsgStatusEnum.read);
+
                                     try {
                                         JSONObject jsonObject = new JSONObject(imMessage.getContent());
                                         long id = jsonObject.getLong("id");
                                         if (id == imAtEntity.id) {
                                             isReaded = imMessage.getStatus() == MsgStatusEnum.read;
-                                            break;
                                         }
                                     } catch (Exception e) {
                                         e.printStackTrace();
                                     }
                                 }
-                                //更新item
-                                at_user_iv.setAlpha(isReaded ? 1.0f : 0.5f);
-                                at_user_tv.setTextColor(isReaded ? READ_TITLE_COLOR : getContextColor(R.color.alpha_font_color_black));
-                                at_content_tv.setTextColor(isReaded ? READ_CONTENT_COLOR : getContextColor(R.color.alpha_font_color_gray));
-
-
-                                if (!isReaded && !TextUtils.isEmpty(imAtEntity.content)) {
-                                    String originalText = imAtEntity.content;
-                                    String targetText = "@[\\w\\u4E00-\\u9FA5\\uF900-\\uFA2D]*";
-                                    SpannableUtils.setTextForegroundColorSpan(at_content_tv,
-                                            originalText,
-                                            targetText,
-                                            SystemUtils.getColor(at_content_tv.getContext(), R.color.alpha_font_color_orange));
-                                } else {
-                                    at_content_tv.setText(imAtEntity.content);
-                                }
+                                updateReadedStateView(isReaded,
+                                        at_user_iv,
+                                        at_user_tv,
+                                        at_content_tv,
+                                        at_from_group_tv,
+                                        at_time_tv,
+                                        imAtEntity);
                             }
                         }
                     });
         } else {
-            at_user_tv.setTextColor(READ_TITLE_COLOR);
-            at_content_tv.setTextColor(READ_CONTENT_COLOR);
-            at_user_iv.setAlpha(0.5f);
+            updateReadedStateView(true,
+                    at_user_iv,
+                    at_user_tv,
+                    at_content_tv,
+                    at_from_group_tv,
+                    at_time_tv,
+                    imAtEntity);
+        }
+    }
+
+    private void updateReadedStateView(boolean isReaded,
+                                       ImageView at_user_iv,
+                                       TextView at_user_tv,
+                                       TextView at_content_tv,
+                                       TextView at_from_group_tv,
+                                       TextView at_time_tv,
+                                       @Nullable IMMessageCustomBody imAtEntity) {
+        if (at_user_iv == null) {
+            return;
+        }
+        int alphaFontColorGray = getContextColor(R.color.alpha_font_color_gray);
+        //更新item
+        at_user_iv.setAlpha(isReaded ? 0.5f : 1.0f);
+        at_user_tv.setTextColor(isReaded ? READED_TITLE_COLOR : getContextColor(R.color.alpha_font_color_black));
+        at_content_tv.setTextColor(isReaded ? READED_CONTENT_COLOR : alphaFontColorGray);
+        at_time_tv.setTextColor(isReaded ? READED_TIME_COLOR : alphaFontColorGray);
+        at_from_group_tv.setTextColor(isReaded ? READED_GROUP_COLOR : alphaFontColorGray);
+
+        if (!isReaded && !TextUtils.isEmpty(imAtEntity.content)) {
+            String originalText = imAtEntity.content;
+            String targetText = "@[\\w\\u4E00-\\u9FA5\\uF900-\\uFA2D]*";
+            SpannableUtils.setTextForegroundColorSpan(at_content_tv,
+                    originalText,
+                    targetText,
+                    SystemUtils.getColor(at_content_tv.getContext(), R.color.alpha_font_color_orange));
+        } else {
+            at_content_tv.setText(imAtEntity.content);
         }
     }
 
